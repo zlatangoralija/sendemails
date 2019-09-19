305ef93031bd2486fc04dbd8e0866b5e95167d56
ZLATANGORALIJA\Zlatan Goralija <zlatangoralija@yahoo.com>
2019-09-19 15:54:03 +0200

    composer update

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::get('/getFailedEmails', 'App\Repositories\Omnitask\SendEmailRepository\SendEmailRepository@getFailedEmails');
-Route::get('/resendEmail/{model_id}/{failed_email_id?}', 'App\Repositories\Omnitask\SendEmailRepository\SendEmailRepository@resendEmail');
+Route::get('/getFailedEmails', 'Omnitask\SendEmailRepository\SendEmailRepository@getFailedEmails');
+Route::get('/resendEmail/{model_id}/{failed_email_id?}', 'Omnitask\SendEmailRepository\SendEmailRepository@resendEmail');
